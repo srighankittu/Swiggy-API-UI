e41662b0260c032ea0aba3959083f12d7f4061f7
srighankittu <107845663+srighankittu@users.noreply.github.com>
2026-01-10 07:29:37 +0530

    hw and rem topics

--- src/components/RestaurantHome.jsx
+++ src/components/RestaurantHome.jsx
@@ -22,9 +22,35 @@ const RestaurantHome = () => {
     fetchSwiggyData();
   }, []);
 
+  // DONE WITH MAJOR REACT STUFF
+
+  // let, var, const, arrays,objects, string(some string operations) map, filter, reduce,
+
+  // basic cats
+  // custom hook,
+  // higher order function
+  // refux store
+  // cart using context --> total item cost
+  // store/ slice --> restaurant data
+  // importance of store
+  // working o store
+  // Redux/rtk-toolkit, Zuestand--> (Veery amazing)
+
+  // Machine coding questions(logic building) : Frontend
+  // Design patterns,  coding patterns, Which is better?
+
+  // DSA a lot of theory questions on OOPS(master), solid --> Backend
+  // List of questions (easy - medium - hard)
+  // 20-25 __> Masters of React(10-1days)
+  // I will solve initially // HW you have solve the same quesion again without seeing my code.
+  // 6th day, medium/hard (zepto(3 pagres , finished payload, including validations), file explorer, tic-tac-toe)
+  // inlclude questions asked in my interviews
+  // easy --> 2 ques per day
+  // Medium and hard -> 2 questions per day
+
   return (
     <div>
-      {restaurants.map((restaurant) => {
+      {restaurants?.map((restaurant) => {
         return <div>{restaurant.info.name}</div>;
       })}
     </div>
